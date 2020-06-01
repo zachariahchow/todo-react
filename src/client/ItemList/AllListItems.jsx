5164143cd37cc859aa4a011a33e8b613c202934c
Zachariah Chow <zachariahchow@gmail.com>
2020-06-01 23:40:57 +0800

    feat: Add AllListItems component

---
+++ src/client/ItemList/AllListItems.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ListItem from './ListItem';
+
+const AllListItems = (props) => {
+
+    const allListItems = props.listItems.map((item) => {
+        return (
+            <ListItem itemName={item.name} itemIndex={item.index}/>
+        );
+    })
+
+    return (
+        <div className="list-items__container">
+            {allListItems}
+        </div>
+    );
+}
+
+
+export default AllListItems;
