7c046e52084db933cbb311c8b9626cf641b822f9
Zachariah Chow <zachariahchow@gmail.com>
2020-06-01 23:41:44 +0800

    feat: Update ItemForm component

--- src/client/ItemForm/ItemForm.jsx
+++ src/client/ItemForm/ItemForm.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ItemFormInput from './ItemFormInput';
+import ItemList from '../ItemList/ItemList';
 
 const ItemForm = (props) => {
+
 
     return (
         <div className="item-form__container">
             <form action="" className="item-form">
-                <ItemFormInput inputtype = "input" name="name" placeholder="Item Name" label="Item Name"/>
-                <button type="button">Add to List</button>
+                <ItemFormInput inputtype = "input" name="name" placeholder="Item Name" label="Item Name" onChange={props.changeHandler}/>
+                <button type="button" onClick={props.clickHandler}>Add to List</button>
             </form>
         </div>
     );
